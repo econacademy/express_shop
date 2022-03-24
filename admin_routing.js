1570e6a49803e24e891f87f1c7d1ae2ec3b59431
System Administrator <root@choegyeongmin-ui-iMac.local>
2022-03-24 15:35:34 +0900

    첫 커밋

---
+++ admin_routing.js
@@ -0,0 +1,14 @@
+//관리자 페이지 관리
+//실제 반환되는 페이지가 있는 url
+//get  "/admin/mem/list/:page" : 회원 리스트
+//get  "/admin/mem/read/:id/form" 회원 상세 && 관리폼
+//get  "/admin/mem/create/form" 회원 등록폼 
+
+//서비스(페이지 반환 없음)
+//get  "/admin/mem/read/:id :  id 중복 확인 
+//get  "/admin/mem/read/:phone : 전화번호 중복확인 
+//put  "/admin/mem/update/" (header 본문) :회원수정
+//delete  "/admin/mem/delete/:id" : 회원 삭제
+//post  "/admin/mem/create/" (header 본문) :회원 등록
+
+
